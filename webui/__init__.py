a243538178a703342e4ba4570dc70ac920ed5b87
Daniel Peixoto Pinto da Silva <peixoto.daniel@hotmail.com>
2020-09-10 14:59:19 -0400

    Alterado o codigo em init.py, removendo alguns if's da função index

--- webui/__init__.py
+++ webui/__init__.py
@@ -20,30 +20,30 @@ from models import User, Dataset
 import os
 
 def hash_and_salt(password):
-    password_hash = hashlib.sha256()
-    salt = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
-    password_hash.update((salt + request.form['password']).encode())
-    return password_hash.hexdigest(), salt
+	password_hash = hashlib.sha256()
+	salt = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
+	password_hash.update((salt + request.form['password']).encode())
+	return password_hash.hexdigest(), salt
 
 
 def require_admin(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if 'username' in session and session['username'] == 'admin':
-            return func(*args, **kwargs)
-        else:
-            return redirect(url_for('webui.login'))
-    return wrapper
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		if 'username' in session and session['username'] == 'admin':
+			return func(*args, **kwargs)
+		else:
+			return redirect(url_for('webui.login'))
+	return wrapper
 
 
 def require_login(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if 'username' in session:
-            return func(*args, **kwargs)
-        else:
-            return redirect(url_for('webui.login'))
-    return wrapper
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		if 'username' in session:
+			return func(*args, **kwargs)
+		else:
+			return redirect(url_for('webui.login'))
+	return wrapper
 
 
 
@@ -53,211 +53,181 @@ webui = Blueprint('webui', __name__, static_folder='static', static_url_path='/s
 @webui.route('/', methods=['GET','POST'])
 @require_login
 def index():
-    if request.method == 'POST':
-        if request.form.get('Valid') == 'Valid':
-            data = Dataset.query.filter_by(file_path =  session['file_path']).first()
-            data.instance_validated= 1
-            data.file_with_user = 0
-            data.instance_valid= 1
-            data.user_validated = session['username']
-            db.session.add(data)
-            db.session.commit()
-        elif request.form.get('Invalid') == 'Invalid1':
-            data = Dataset.query.filter_by(file_path =  session['file_path']).first()
-            data.instance_validated= 1
-            data.file_with_user = 0
-            data.instance_valid= -1
-            data.user_validated = session['username']
-            db.session.add(data)
-            db.session.commit()
-        elif request.form.get('Invalid') == 'Invalid2':
-            data = Dataset.query.filter_by(file_path =  session['file_path']).first()
-            data.instance_validated= 1
-            data.file_with_user = 0
-            data.instance_valid= -2
-            data.user_validated = session['username']
-            db.session.add(data)
-            db.session.commit()
-        elif request.form.get('Invalid') == 'Invalid3':
-            data = Dataset.query.filter_by(file_path =  session['file_path']).first()
-            data.instance_validated= 1
-            data.file_with_user = 0
-            data.instance_valid= -3
-            data.user_validated = session['username']
-            db.session.add(data)
-            db.session.commit()
-        elif request.form.get('Invalid') == 'Invalid4':
-            data = Dataset.query.filter_by(file_path =  session['file_path']).first()
-            data.instance_validated= 1
-            data.file_with_user = 0
-            data.instance_valid= -4
-            data.user_validated = session['username']
-            db.session.add(data)
-            db.session.commit()
-        elif request.form.get('Invalid') == 'Invalid5':
-            data = Dataset.query.filter_by(file_path =  session['file_path']).first()
-            data.instance_validated= 1
-            data.file_with_user = 0
-            data.instance_valid= -5
-            data.user_validated = session['username']
-            db.session.add(data)
-            db.session.commit()
+	if request.method == 'POST':
+		
+		if request.form.get('Valid') == 'Valid':
+			data = Dataset.query.filter_by(file_path =  session['file_path']).first()
+			data.instance_validated= 1
+			data.file_with_user = 0
+			data.instance_valid= 1
+			data.user_validated = session['username']
+			db.session.add(data)
+			db.session.commit()
+		elif request.form.get('Invalid')[:-1] == 'Invalid':
+			invalidClass = -int(request.form.get('Invalid')[-1])
+			data = Dataset.query.filter_by(file_path =  session['file_path']).first()
+			data.instance_validated= 1
+			data.file_with_user = 0
+			data.instance_valid= invalidClass
+			data.user_validated = session['username']
+			db.session.add(data)
+			db.session.commit()
+		
+		return redirect(url_for('webui.index'))
 
-        return redirect(url_for('webui.index'))
+	data = Dataset.query.filter_by(instance_validated=0,file_with_user=0).first()
+	if data is None:
+		return render_template('index-finish.html')
+	else:
+		data.file_with_user = 1
+		session['text'] = data.text
+		session['audio_lenght'] = data.audio_lenght
+		session['file_path']= data.file_path
+		db.session.add(data)
+		db.session.commit()
+		
+		data.file_path=os.path.join('Dataset',data.file_path)
 
-    data = Dataset.query.filter_by(instance_validated=0,file_with_user=0).first()
-    if data is None:
-        return render_template('index-finish.html')
-    else:
-        data.file_with_user = 1
-        session['text'] = data.text
-        session['audio_lenght'] = data.audio_lenght
-        session['file_path']= data.file_path
-        db.session.add(data)
-        db.session.commit()
-        
-        data.file_path=os.path.join('Dataset',data.file_path)
-
-        return render_template('index.html',dataset=data)
+		return render_template('index.html',dataset=data)
 
 
 @webui.route('/tutorial', methods=['GET', 'POST'])
 def tutorial():
-    if request.method == 'POST':
-        return redirect(url_for('webui.index'))
-        #if request.form.get('sairtutorial') == 'Ir para Anotação':
-    return render_template('tutorial.html')
-    
+	if request.method == 'POST':
+		return redirect(url_for('webui.index'))
+		#if request.form.get('sairtutorial') == 'Ir para Anotação':
+	return render_template('tutorial.html')
+	
 
 @webui.route('/admin', methods=['GET', 'POST'])
 @require_admin
 def admin():
-    instances = []
-    if request.method == 'POST':
-        if request.form.get('Download Valid instances') == 'Download Valid instances':
-            data = Dataset.query.filter_by(instance_valid=1)
-            csv = ""
-            for dt in data: 
-                string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
-                csv+=string
-            return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=valid_instances.csv"})
-        elif request.form.get("Download Invalid instances 1") == "Download Invalid instances 1":
-            data = Dataset.query.filter_by(instance_valid=-1,instance_validated=1)
-            csv = ""
-            for dt in data: 
-                string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
-                csv+=string
-            return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=invalid_instances_1.csv"})
-        elif request.form.get("Download Invalid instances 2") == "Download Invalid instances 2":
-            data = Dataset.query.filter_by(instance_valid=-2,instance_validated=1)
-            csv = ""
-            for dt in data: 
-                string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
-                csv+=string
-            return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=invalid_instances_2.csv"})
-        elif request.form.get("Download Invalid instances 3") == "Download Invalid instances 3":
-            data = Dataset.query.filter_by(instance_valid=-3,instance_validated=1)
-            csv = ""
-            for dt in data: 
-                string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
-                csv+=string
-            return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=invalid_instances_3.csv"})
-        elif request.form.get("Download Invalid instances 4") == "Download Invalid instances 4":
-            data = Dataset.query.filter_by(instance_valid=-4,instance_validated=1)
-            csv = ""
-            for dt in data: 
-                string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
-                csv+=string
-            return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=invalid_instances_4.csv"})
-        elif request.form.get("Download Invalid instances 5") == "Download Invalid instances 5":
-            data = Dataset.query.filter_by(instance_valid=-5,instance_validated=1)
-            csv = ""
-            for dt in data: 
-                string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
-                csv+=string
-            return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=invalid_instances_5.csv"})
-    return render_template('admin.html')
+	instances = []
+	if request.method == 'POST':
+		if request.form.get('Download Valid instances') == 'Download Valid instances':
+			data = Dataset.query.filter_by(instance_valid=1)
+			csv = ""
+			for dt in data: 
+				string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
+				csv+=string
+			return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=valid_instances.csv"})
+		elif request.form.get("Download Invalid instances 1") == "Download Invalid instances 1":
+			data = Dataset.query.filter_by(instance_valid=-1,instance_validated=1)
+			csv = ""
+			for dt in data: 
+				string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
+				csv+=string
+			return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=invalid_instances_1.csv"})
+		elif request.form.get("Download Invalid instances 2") == "Download Invalid instances 2":
+			data = Dataset.query.filter_by(instance_valid=-2,instance_validated=1)
+			csv = ""
+			for dt in data: 
+				string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
+				csv+=string
+			return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=invalid_instances_2.csv"})
+		elif request.form.get("Download Invalid instances 3") == "Download Invalid instances 3":
+			data = Dataset.query.filter_by(instance_valid=-3,instance_validated=1)
+			csv = ""
+			for dt in data: 
+				string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
+				csv+=string
+			return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=invalid_instances_3.csv"})
+		elif request.form.get("Download Invalid instances 4") == "Download Invalid instances 4":
+			data = Dataset.query.filter_by(instance_valid=-4,instance_validated=1)
+			csv = ""
+			for dt in data: 
+				string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
+				csv+=string
+			return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=invalid_instances_4.csv"})
+		elif request.form.get("Download Invalid instances 5") == "Download Invalid instances 5":
+			data = Dataset.query.filter_by(instance_valid=-5,instance_validated=1)
+			csv = ""
+			for dt in data: 
+				string = dt.file_path+','+str(dt.audio_lenght)+','+dt.text+'\n'
+				csv+=string
+			return Response(csv,mimetype="text/csv", headers={"Content-disposition":"attachment; filename=invalid_instances_5.csv"})
+	return render_template('admin.html')
 
 
 @webui.route('/login', methods=['GET', 'POST'])
 def login():
-    admin_user = User.query.filter_by(username='admin').first()
-    if not admin_user:
-        if request.method == 'POST':
-            if 'password' in request.form:
-                password_hash, salt = hash_and_salt(request.form['password']) 
-                new_user = User()
-                new_user.username = 'admin'
-                new_user.password = password_hash
-                new_user.salt = salt
-                db.session.add(new_user)
-                db.session.commit()
-                flash('Password set successfully. Please log in.')
-                return redirect(url_for('webui.login'))
-        return render_template('create_password.html')
-    if request.method == 'POST':
-        if request.form['password'] and request.form['username']:
-                user = User.query.filter_by(username=request.form['username']).first()
-                password_hash = hashlib.sha256()
-                password_hash.update((user.salt + request.form['password']).encode())
-                if user is  not None:
-                    if user.password == password_hash.hexdigest():
-                        session['username'] = request.form['username']
-                        last_login_time =  user.last_login_time
-                        last_login_ip = user.last_login_ip
-                        user.last_login_time = datetime.now()
-                        user.last_login_ip = request.remote_addr
-                        db.session.commit()
-                        flash('Logged in successfully.') 
-                        if last_login_ip:
-                            flash('Last login from ' + last_login_ip + ' on ' + last_login_time.strftime("%d/%m/%y %H:%M"))
-                        if session['username'] == 'admin':
-                            return redirect(url_for('webui.admin'))
-                        else:
-                            return redirect(url_for('webui.index'))
-                    else:
-                        flash('Wrong password')
-                else:
-                    flash('This user is not registered. Contact an administrator !')
-    return render_template('login.html')
+	admin_user = User.query.filter_by(username='admin').first()
+	if not admin_user:
+		if request.method == 'POST':
+			if 'password' in request.form:
+				password_hash, salt = hash_and_salt(request.form['password']) 
+				new_user = User()
+				new_user.username = 'admin'
+				new_user.password = password_hash
+				new_user.salt = salt
+				db.session.add(new_user)
+				db.session.commit()
+				flash('Password set successfully. Please log in.')
+				return redirect(url_for('webui.login'))
+		return render_template('create_password.html')
+	if request.method == 'POST':
+		if request.form['password'] and request.form['username']:
+				user = User.query.filter_by(username=request.form['username']).first()
+				password_hash = hashlib.sha256()
+				password_hash.update((user.salt + request.form['password']).encode())
+				if user is  not None:
+					if user.password == password_hash.hexdigest():
+						session['username'] = request.form['username']
+						last_login_time =  user.last_login_time
+						last_login_ip = user.last_login_ip
+						user.last_login_time = datetime.now()
+						user.last_login_ip = request.remote_addr
+						db.session.commit()
+						flash('Logged in successfully.') 
+						if last_login_ip:
+							flash('Last login from ' + last_login_ip + ' on ' + last_login_time.strftime("%d/%m/%y %H:%M"))
+						if session['username'] == 'admin':
+							return redirect(url_for('webui.admin'))
+						else:
+							return redirect(url_for('webui.index'))
+					else:
+						flash('Wrong password')
+				else:
+					flash('This user is not registered. Contact an administrator !')
+	return render_template('login.html')
 
 
 @webui.route('/passchange', methods=['GET', 'POST'])
 @require_login
 def change_password():
-    if request.method == 'POST':
-        if 'password' in request.form:
-            admin_user = User.query.filter_by(username=session['username']).first()
-            password_hash, salt = hash_and_salt(request.form['password'])
-            admin_user.password = password_hash
-            admin_user.salt = salt
-            db.session.add(admin_user)
-            db.session.commit()
-            flash('Password reset successfully. Please log in.')
-            return redirect(url_for('webui.login'))
-    return render_template('create_password.html')
+	if request.method == 'POST':
+		if 'password' in request.form:
+			admin_user = User.query.filter_by(username=session['username']).first()
+			password_hash, salt = hash_and_salt(request.form['password'])
+			admin_user.password = password_hash
+			admin_user.salt = salt
+			db.session.add(admin_user)
+			db.session.commit()
+			flash('Password reset successfully. Please log in.')
+			return redirect(url_for('webui.login'))
+	return render_template('create_password.html')
 
 @webui.route('/adduser', methods=['GET', 'POST'])
 @require_admin
 def add_user():
-    if request.method == 'POST':
-        if 'password' in request.form and 'username' in request.form:
-            print(request.form['password'])
-            password_hash, salt = hash_and_salt(request.form['password']) 
-            new_user = User()
-            new_user.username = request.form['username']
-            new_user.password = password_hash
-            new_user.salt = salt
-            db.session.add(new_user)
-            db.session.commit()
-            flash('User '+request.form['username']+ ' successfully registered')
-            return redirect(url_for('webui.admin'))
-    return render_template('create_user.html')
+	if request.method == 'POST':
+		if 'password' in request.form and 'username' in request.form:
+			print(request.form['password'])
+			password_hash, salt = hash_and_salt(request.form['password']) 
+			new_user = User()
+			new_user.username = request.form['username']
+			new_user.password = password_hash
+			new_user.salt = salt
+			db.session.add(new_user)
+			db.session.commit()
+			flash('User '+request.form['username']+ ' successfully registered')
+			return redirect(url_for('webui.admin'))
+	return render_template('create_user.html')
 
 
 @webui.route('/logout')
 def logout():
-    session.pop('username', None)
-    flash('Logged out successfully.')
-    return redirect(url_for('webui.login'))
+	session.pop('username', None)
+	flash('Logged out successfully.')
+	return redirect(url_for('webui.login'))
 
