c03e67242d07d459bc1d0fcc364ae2399f89b47a
Daniel Peixoto Pinto da Silva <peixoto.daniel@hotmail.com>
2021-07-25 23:26:01 -0300

    Alterado path do python para rodar o script generate_csv.py

--- webui/controllers/admin_controller.py
+++ webui/controllers/admin_controller.py
@@ -3,4 +3,5 @@ import os
 class AdminController:
 
 	def generate_csv(self):
-		os.system("python ./webui/utils/generate_csv.py &")
+		#trocar path pelo o path do seu python quando for testar
+		os.system("/home/utf/miniconda3/bin/python3 ./webui/utils/generate_csv.py &")
